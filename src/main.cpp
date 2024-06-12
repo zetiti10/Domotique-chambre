7549b2ad598df21cf8fde1c0a1715367bfe78c4b
Louis Léculier <louis.leculier@gmail.com>
2024-06-12 17:45:46 +0200

    Musique à tester

--- src/main.cpp
+++ src/main.cpp
@@ -98,8 +98,8 @@ void setup()
     LEDStrip.setMode(&colorMode);
 
     // Liste des musiques.
-    const static Music *const musicList[] PROGMEM = {&test1, &test2};
-    int musicsNumber = 2;
+    const static Music *const musicList[] PROGMEM = {&worldsSmallestViolinMusic};
+    int musicsNumber = 1;
 
     // Création d'une liste contenant des références vers tous les actionneurs utilisés par le système de musique animée.
     Output *outputList[] = {&tray, &LEDCube, &disco, &beacon, &wardrobeLights, &street, &deskLight, &doorLED, &LEDStrip, &alarm, &mainLights, &sofaLight, &bedLight, &cameraLight};
--- src/musics.hpp
+++ src/musics.hpp
@@ -3,43 +3,39 @@
 
 #include "device/output/television.hpp"
 
-const Action test1Music[] PROGMEM = {
-    {8800, "09011000000000255255255010001"},
-    {9800, "09011255255255000000000010001"},
-    {11000, "09011000000000255255255010003"},
-    {17000, "02001"},
-    {17000, "090122552552550100"},
-    {20000, "09011255100000000200255030003"},
-    {25000, "03001"},
-    {25000, "02000"},
-    {30000, "03000"},
+const Action worldsSmallestViolinActions[] PROGMEM = {
+    {3900, "09011000000000255255255041002"},
+    {8000, "09000"},
+    {9000, "96001"},
+    {11000, "96000"},
+    {11001, "97001"},
+    {13000, "97000"},
+    {13001, "98001"},
+    {11000, "96000"},
+    {15000, "09011000050050255000000020003"},
+    {17500, "09010200000000"},
+    {19225, "09010125000000"},
+    {21225, "09010050000000"},
+    {23225, "01001"},
+    {25000, "04001"},
+    {27400, "05001"},
+    {41750, "97021050"},
+    {41751, "090120342550890255"},
+    {49600, "03001"},
+    {58000, "01000"},
+    {58001, "02000"},
+    {58002, "03000"},
+    {58003, "04000"},
+    {58004, "09000"},
+    {58005, "97000"},
+    {58005, "97000"},
 };
-const char friendlyName1[] PROGMEM = "Test PPAP";
-const char videoURL1[] PROGMEM = "https://www.udrop.com/";
-
-const Music test1 PROGMEM = {
-    friendlyName1,
-    videoURL1,
-    test1Music,
-    9};
-
-const Action test2Music[] PROGMEM = {
-    {1600, "03001"},
-    {3600, "03000"},
-    {5600, "03001"},
-    {7600, "03000"},
-    {8600, "03001"},
-    {9600, "03000"},
-    {10600, "09011000000000255255255010001"},
-    {11600, "09011255255255000000000010001"},
-};
-const char friendlyName2[] PROGMEM = "Test Couleurs";
-const char videoURL2[] PROGMEM = "https://www.udrop.com/";
-
-const Music test2 PROGMEM = {
-    friendlyName2,
-    videoURL2,
-    test2Music,
-    8};
+const char worldsSmallestViolinName[] PROGMEM = "Test PPAP";
+const char worldsSmallestViolinVideoURL[] PROGMEM = "https://www.udrop.com/LLUx/Worlds_Smallest_Violin.mp4?download_token=8db514c04dd1eb8aab4381e5dbfbe91481864bec379e8640c5b0058d230bfb01";
+const Music worldsSmallestViolinMusic PROGMEM = {
+    worldsSmallestViolinName,
+    worldsSmallestViolinVideoURL,
+    worldsSmallestViolinActions,
+    25};
 
 #endif
